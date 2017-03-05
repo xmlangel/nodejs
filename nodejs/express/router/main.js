919ad5f387fd20ddd6220508bd398d5d572184a9
kwangmyung Kim <kmkim@whatap.io>
2017-03-05 22:31:47 +0900

    get list

--- nodejs/express/router/main.js
+++ nodejs/express/router/main.js
@@ -6,4 +6,12 @@ module.exports = function(app, fs)
              length: 5
          })
      });
+
+     app.get('/list', function (req, res) {
+       fs.readFile( __dirname + "/../data/" + "user.json", 'utf8', function (err, data) {
+           console.log( data );
+           res.end( data );
+       });
+    })
+
 }
