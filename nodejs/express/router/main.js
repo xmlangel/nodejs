b487203f61189aeb125a28c86218b321c61ef2f0
kwangmyung Kim <kmkim@whatap.io>
2017-03-05 23:11:43 +0900

    Login

--- nodejs/express/router/main.js
+++ nodejs/express/router/main.js
@@ -107,5 +107,38 @@ module.exports = function(app, fs)
             })
         })
  
-    })
+    });
+
+        app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+ 
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+            if(!users[username]){
+                // USERNAME NOT FOUND
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+ 
+            if(users[username]["password"] == password){
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+ 
+            }else{
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    });
+
+        
 }
