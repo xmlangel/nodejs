beb1116c9339efa854d1c8a863895a02ff82eac8
kwangmyung Kim <kmkim@whatap.io>
2017-03-05 22:46:51 +0900

    PUT updateUser/:username

--- nodejs/express/router/main.js
+++ nodejs/express/router/main.js
@@ -55,4 +55,32 @@ module.exports = function(app, fs)
             })
         })
     });
+
+        app.put('/updateUser/:username', function(req, res){
+ 
+        var result = {  };
+        var username = req.params.username;
+ 
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+ 
+        // LOAD DATA
+        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+            var users = JSON.parse(data);
+            // ADD/MODIFY DATA
+            users[username] = req.body;
+ 
+            // SAVE DATA
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result = {"success": 1};
+                res.json(result);
+            })
+        })
+    });
 }
